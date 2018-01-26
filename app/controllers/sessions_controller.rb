cc33346e6d57d0b596375d0c1d0b479a440a564a
kirandeshmukh <kirand@joshsoftware.com>
2018-01-26 16:46:07 +0530

    [FarmAtHome] Adds application logic to manage inventories
    -Added model, view, controller and test cases for inventories management

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,8 +4,8 @@ class SessionsController < Devise::SessionsController
   end
 
   def create
-    user = User.find_by(email: params[:email])
-    if user && user.valid_password?(params[:password])
+    user = User.find_by(email: params[:user][:email])
+    if user && user.valid_password?(params[:user][:password])
       sign_in user
     end
   end
