715b565252eed8244cf347fb6153e2a95167e4b1
kirandeshmukh <kirand@joshsoftware.com>
2018-01-26 16:43:53 +0530

    [FarmAtHome] Adds useful db tables in schema file

--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,23 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180121095350) do
+ActiveRecord::Schema.define(version: 20180121114914) do
 
   create_table "categories", force: :cascade do |t|
     t.string "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+  end
+
+  create_table "inventories", force: :cascade do |t|
+    t.integer "item_id"
+    t.integer "category_id"
+    t.integer "quantity"
+    t.float "price"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["category_id"], name: "index_inventories_on_category_id"
+    t.index ["item_id"], name: "index_inventories_on_item_id"
   end
 
   create_table "items", force: :cascade do |t|
